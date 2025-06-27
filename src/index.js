4ba3292ae9b9c156ad5e48d0ea515349d554cee5
Luanny Dutra <luannyrd2017@gmail.com>
2025-06-27 01:44:39 -0300

    De novo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; // aqui importa do src/
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -10,15 +12,4 @@ root.render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceWorkerRegistration.js")  
-      .then((registration) => {
-        console.log("Service Worker registrado:", registration);
-      })
-      .catch((error) => {
-        console.error("Erro ao registrar Service Worker:", error);
-      });
-  });
-}
+serviceWorkerRegistration.register();
